2c5deb2d8b86904186fb1b7141d831e74d9cef6e
Rosita Bell <rbell@ox-u1.msk.21-school.ru>
2021-11-24 10:48:49 +0300

    return ptr fixed

--- print_ft/print_ptr.c
+++ print_ft/print_ptr.c
@@ -28,20 +28,38 @@ static size_t	print_null_ptr(t_flags *flgs)
 	return ((size_t)flgs->width);
 }
 
+static size_t	count_ptr_size(unsigned long int p)
+{
+	size_t	size;
+
+	size = 2;
+	while (p > 0)
+	{
+		p /= 16;
+		size++;
+	}
+	return (size);
+}
+
+static size_t print_ptr_return(unsigned long int p, t_flags *flgs, size_t size)
+{
+	print_no_width_x(p, flgs, "0123456789abcdef", 1);
+	return (size);
+}
+
 size_t	print_ptr(va_list *arg, t_flags *flgs)
 {
 	unsigned long int	p;
 	int					i;
+	size_t				size;
 
 	i = -1;
 	p = va_arg(*arg, unsigned long int);
 	if (!p)
 		return (print_null_ptr(flgs));
-	if (flgs->width <= 14)
-	{
-		print_no_width_x(p, flgs, "0123456789abcdef", 1);
-		return (14);
-	}
+	size = count_ptr_size(p);
+	if ((size_t)(flgs->width) < size)
+		return (print_ptr_return(p, flgs, size));
 	if (flgs->f_minus)
 	{
 		print_no_width_x(p, flgs, "0123456789abcdef", 1);
